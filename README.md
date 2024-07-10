50948f16180b7d4a0256e9c04255e0585a2cbace
teaaqueenn <the.teaa.queenn@gmail.com>
2024-07-11 00:02:17 +0800

    Initial Commit + Created the method for solving the cube
    I physically checked it to see if it works and the solving part works perfectly. Next step is to integrate computer vision.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rubix-cube
+
+This is basically a fun project I worked on in order to work on computer vision.
---
+++ rubixCubeSolver.py
@@ -0,0 +1,74 @@
+import kociemba
+
+'''
+white = U 
+blue = L 
+orange = F 
+green = R 
+red = B 
+yellow = D
+'''
+
+#Variables + Functions
+max_steps = 20
+
+def turn_motor(move):
+    print(f"Turning motor for move: {move}")
+
+#Cube stuff
+topSide = 'UUUUUUUUU'
+rightSide ='FBFRRRRRR'
+frontSide = 'LRLFFFFFF'
+bottomSide = 'DDDDDDDDD'
+leftSide = 'BFBLLLLLL'
+backSide ='RLRBBBBBB'
+cube = topSide + rightSide + frontSide + bottomSide + leftSide + backSide
+
+print(cube)
+
+solution = kociemba.solve(cube)
+
+steps = solution.split(" ")
+
+for step in steps:
+    match step:
+        case 'U':
+            turn_motor('U')
+        case 'U2':
+            turn_motor('U2')
+        case 'U\'':
+            turn_motor('U\'')
+        case 'D':
+            turn_motor('D')
+        case 'D2':
+            turn_motor('D2')
+        case 'D\'':
+            turn_motor('D\'')
+        case 'L':
+            turn_motor('L')
+        case 'L2':
+            turn_motor('L2')
+        case 'L\'':
+            turn_motor('L\'')
+        case 'R':
+            turn_motor('R')
+        case 'R2':
+            turn_motor('R2')
+        case 'R\'':
+            turn_motor('R\'')
+        case 'F':
+            turn_motor('F')
+        case 'F2':
+            turn_motor('F2')
+        case 'F\'':
+            turn_motor('F\'')
+        case 'B':
+            turn_motor('B')
+        case 'B2':
+            turn_motor('B2')
+        case 'B\'':
+            turn_motor('B\'')
+        case _:
+            print(f"Unknown move type: {step}")
+
+
